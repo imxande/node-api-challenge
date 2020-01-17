b5d654aad98900a54fd69cd2dc90bb8f382f8a37
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 17:50:46 -0500

    added action delete, update

--- actionsRouter/router.js
+++ actionsRouter/router.js
@@ -1,7 +1,7 @@
 // import express just like React
 const express = require("express");
 
-// import the projects data base
+// import the Actions data base
 const ActionDataBase = require("../data/helpers/actionModel.js");
 
 // we need to use express Router goody 
@@ -15,7 +15,7 @@ const router = express.Router();
 // Getting all actions here
 router.get("/", (req, res) => {
 
-    // Using get function from the data base to get all projects
+    // Using get function from the data base to get all Actions
     ActionDataBase.get()
     .then(response=> {
         res.status(200).json(response);
@@ -29,7 +29,7 @@ router.get("/", (req, res) => {
 });
 
 
-// to create a new project
+// to create a new Action
 router.post("/:id", (req, res) =>{
 
      // I need to check if we have the id firts
@@ -57,6 +57,49 @@ router.post("/:id", (req, res) =>{
     })
 })
 
+// / now i want to update the Action
+router.put("/:id", (req, res) =>{
+    // I need to check if we have the id firts
+    if(!req.params.id){
+        // if no id then return a 404
+        return res.status(404).json({errorMessage:"Action with such ID does not exist"})
+    }
+
+    else if(!req.body.name || !req.body.description){
+        // return a bad request
+        return res.status(400).json({
+            errorMessage: "Please provide name and description for new a Action"
+        })
+    }
+
+    // im going to use update function provided to update the Action
+    ActionDataBase.update(req.params.id, req.body)
+        .then(response =>{
+            // return successful status if it works 
+            return res.status(200).json(response)
+        })
+        .catch(error =>{
+            console.log(error)
+            res.status(500).json("There is an error in the server while trying to update the Action")
+        })
+})
+
+// need to delete Action here
+router.delete("/:id", (req,res) =>{
+     // I need to check if we have the id firts
+     if(!req.params.id){
+        // if no id then return a 404
+        return res.status(404).json({errorMessage:"Action with such ID does not exist"})
+    }
+    ActionDataBase.remove(req.params.id)
+        .then(response =>{
+            return res.status(200).json({message : "Action deleted"})
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json("There is an error in the server while trying to delete the Action")
+        })
+})
 
 
 // exporting router here
