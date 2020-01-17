abbca6dbbd5d473c8d8eab9e88fa2b10444cc088
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 16:35:52 -0500

    post request for project working

--- projectRoute/router.js
+++ projectRoute/router.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 // i need to get all the projects here
 router.get("/", (req, res) => {
-    
+
     // Using get function from the data base to get all projects
     ProjectDataBase.get()
     .then(response=> {
@@ -28,6 +28,27 @@ router.get("/", (req, res) => {
     });
 });
 
+// to create a new project
+router.post("/", (req, res) =>{
+
+    // checking for name and description here
+    if(!req.body.name || !req.body.description){
+        // return a bad request
+        return res.status(400).json({
+            errorMessage: "Please provide name and description for new a project"
+        })
+    }
+    // i need to use insert function here
+    ProjectDataBase.insert(req.body)
+    .then(response =>{
+        // return a create is successful 
+        return res.status(200).json(response)
+    })
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json("There is an error in the server")
+    })
+})
 
 // we need to export router (dont forget the s for exports)
 module.exports = router;
