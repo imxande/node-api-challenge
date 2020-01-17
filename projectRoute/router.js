d511f2aac195a258992eb6b580c21dc94163ced9
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 13:53:21 -0500

    all projects logic but I have a problem with sqlite3

--- projectRoute/router.js
+++ projectRoute/router.js
@@ -7,9 +7,27 @@ const ProjectDataBase = require("../data/helpers/projectModel.js");
 // we need to use express Router goody 
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.send("Hello from the router");
-})
+// checkin if router is working
+// router.get("/", (req, res) =>{
+//     res.send("Hello from the router");
+// })
+
+// i need to get all the projects here
+router.get("/", (req, res) => {
+    
+    // Using get function from the data base to get all projects
+    ProjectDataBase.get()
+    .then(response=> {
+        res.status(200).json(response);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: "Ok something went wrong with the server"
+        });
+    });
+});
+
 
 // we need to export router (dont forget the s for exports)
 module.exports = router;
