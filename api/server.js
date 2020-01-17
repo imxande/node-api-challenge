27b2af3b8215e09a7da7f299f2272d8ba1ab59b6
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 13:06:38 -0500

    server is running

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ server.use(express.json());
 
 // checking if server is listening
 server.get('/', (req, res) =>{
-    res.send(`<h1> Any body home? </h1>`)
+    res.send(`<h1> Any body home? </h1> <p>Ok server is running now</p>`)
 })
 
 // exporting the server to use it on index
