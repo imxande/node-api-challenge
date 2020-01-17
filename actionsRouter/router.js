330d03e72d1dc7c80e7c168a6fe5e46fb3683e49
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 17:41:07 -0500

    added post for action

--- actionsRouter/router.js
+++ actionsRouter/router.js
@@ -29,6 +29,34 @@ router.get("/", (req, res) => {
 });
 
 
+// to create a new project
+router.post("/:id", (req, res) =>{
+
+     // I need to check if we have the id firts
+     if(!req.params.id){
+        // if no id then return a 404
+        return res.status(404).json({errorMessage:"Action with such ID does not exist"})
+    }
+
+    // checking for name and description here
+    else if(!req.body.description || !req.body.notes){
+        // return a bad request
+        return res.status(400).json({
+            errorMessage: "Please provide description and notes for new action"
+        })
+    }
+    // i need to use insert function here
+    ActionDataBase.insert(req.body)
+    .then(response =>{
+        // return a create is successful 
+        return res.status(200).json(response)
+    })
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json("There is an error in the server")
+    })
+})
+
 
 
 // exporting router here
