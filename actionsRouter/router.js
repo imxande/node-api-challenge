b4db371475ec5785961e88d848f10159936d84a3
imxande <alerodriguezlarosa@gmail.com>
2020-01-17 17:17:12 -0500

    getting all actions now

--- actionsRouter/router.js
+++ actionsRouter/router.js
@@ -7,10 +7,27 @@ const ActionDataBase = require("../data/helpers/actionModel.js");
 // we need to use express Router goody 
 const router = express.Router();
 
-// checkin if router is working
-router.get("/", (req, res) =>{
-    res.send("Hello from the action router");
-})
+// // checkin if router is working
+// router.get("/", (req, res) =>{
+//     res.send("Hello from the action router");
+// })
+
+// Getting all actions here
+router.get("/", (req, res) => {
+
+    // Using get function from the data base to get all projects
+    ActionDataBase.get()
+    .then(response=> {
+        res.status(200).json(response);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: "Ok something went wrong with the server"
+        });
+    });
+});
+
 
 
 
